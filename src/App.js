7dd586093812ecba3de492730ef2f2c8d20356d9
MayuraPabasara <mayurapabasara143@gmail.com>
2025-11-23 11:31:10 +0530

    update Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
-function App() {
+const App = () => {
+
+  const [imageUrl, setImageUrl] = useState("")
+  const [name, setName] = useState("")
+  const [city, setCity] = useState("")
+  const [position, setPosition] = useState("")
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div className="main_container">
+
+      <div className="main_left"> 
+
+          <input type="text" value={imageUrl} onChange={(e)=>{
+            e.preventDefault()
+            setImageUrl(e.target.value)
+          }}/>
+
+          <input type="text" value={name} onChange={(e)=>{
+            e.preventDefault()
+            setName(e.target.value)
+          }}/>
+
+          <input type="text" value={city} onChange={(e)=>{
+            e.preventDefault()
+            setCity(e.target.value)
+          }}/>
+
+          <input type="text" value={position} onChange={(e)=>{
+            e.preventDefault()
+            setPosition(e.target.value)
+          }}/>
+
+          <button className="main_button" onClick={()=>console.log({imageUrl, name, city, position})}>Submit</button>
+      </div>
+
+      <div className="main_right"></div>
+
+  </div>
+  )
 }
 
-export default App;
+export default  App;
